d4c7dffbe58f6bf0dee5cd533216e59fc1c4d138
Akshaya882 <nlakshaya2005@gmail.com>
2025-06-22 01:34:56 +0530

    Update mongo.php

--- db/mongo.php
+++ db/mongo.php
@@ -1,10 +1,15 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php'; // Composer autoload
+require_once __DIR__ . '/../vendor/autoload.php'; // Composer autoload
 
-$mongoClient = new MongoDB\Client("mongodb://localhost:27017");
-
-// Database: guvi_profiles
-// Collection: profiles
-$mongoDB = $mongoClient->guvi_profiles;
-$profileCollection = $mongoDB->profiles;
+try {
+    $mongoClient = new MongoDB\Client("mongodb://localhost:27017");
+    $mongoDB = $mongoClient->selectDatabase("guvi_intern");
+    $mongoCollection = $mongoDB->selectCollection("user_profiles");
+} catch (Exception $e) {
+    echo json_encode([
+        "status" => "error",
+        "message" => "MongoDB connection failed: " . $e->getMessage()
+    ]);
+    exit();
+}
 ?>
