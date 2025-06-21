9ffec19c3bcf6aa2d325657f895da0b11b1c6563
Akshaya882 <nlakshaya2005@gmail.com>
2025-06-22 01:58:54 +0530

    Update test-db.php

--- php/test-db.php
+++ php/test-db.php
@@ -2,6 +2,6 @@
 require_once('../db/mysql.php');
 require_once('../db/mongo.php');
 
-echo "<h3>✅ MySQL Connected</h3>";
-echo "<h3>✅ MongoDB Connected</h3>";
+echo "<h3> MySQL Connected</h3>";
+echo "<h3> MongoDB Connected</h3>";
 ?>
