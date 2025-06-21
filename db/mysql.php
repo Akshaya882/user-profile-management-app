14a48b2276d49619b1c78ea8919f2479deda61d1
Akshaya882 <nlakshaya2005@gmail.com>
2025-06-22 01:35:35 +0530

    Update mysql.php

--- db/mysql.php
+++ db/mysql.php
@@ -1,13 +1,20 @@
 <?php
-$host = "localhost";
-$port = 3307;
-$dbname = "guvi_intern";
-$username = "root";
-$password = "";
+require __DIR__ . '/../vendor/autoload.php';
 
-$conn = new mysqli($host, $username, $password, $dbname, $port);
+use Dotenv\Dotenv;
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+$name = $_ENV['DB_NAME'];
+$port = $_ENV['DB_PORT'];
+
+$mysql_conn = new mysqli($host, $user, $pass, $name, $port);
+
+if ($mysql_conn->connect_error) {
+    die("Connection failed: " . $mysql_conn->connect_error);
 }
 ?>
